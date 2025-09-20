a127a31da81d9fb39e414d937f3fdec7c93db41b
KSultonboy <132754862+KSultonboy@users.noreply.github.com>
2025-09-20 11:06:17 +0500

    fully developed

--- app.config.ts
+++ app.config.ts
@@ -6,19 +6,34 @@ import type { ExpoConfig } from "expo/config";
 
 export default (): ExpoConfig => ({
     name: "SavdoHisobi",
-    slug: "SavdoHisobi",
+    slug: "savdohisobi",
     scheme: "wholesaleapp",
     version: "1.0.0",
 
     experiments: { typedRoutes: true },
 
     android: {
-        package: "com.ruhshonatortapps.wholesaleapp"
+        package: "com.ruhshonatortapps.SavdoHisobi",
+        "versionCode": 1
     },
 
     ios: {
-        bundleIdentifier: "com.ruhshonatortapps.wholesaleapp"
+        bundleIdentifier: "com.ruhshonatortapps.SavdoHisobi"
     },
+
+    updates: {
+        enabled: true,
+        checkAutomatically: "ON_LOAD",
+        fallbackToCacheTimeout: 0,
+        url: "https://u.expo.dev/1333c9b1-8910-4a25-9354-97cf2d39be36", // ← EAS Update URL
+    },
+
+
+    runtimeVersion: "1.0.0", // <<<<<<<<< MUHIM: bare workflow uchun qat’iy satr
+    
+
+    "owner": "ruhshonatortapps",
+
 
     icon: "./assets/App-icon.png",
     splash: {
@@ -31,7 +46,7 @@ export default (): ExpoConfig => ({
         SUPABASE_URL: process.env.SUPABASE_URL,
         SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
         eas: {
-            projectId: "185af514-ac28-4e82-985a-1b41493826fd",
+            projectId: "1333c9b1-8910-4a25-9354-97cf2d39be36",
         },
     },
 
